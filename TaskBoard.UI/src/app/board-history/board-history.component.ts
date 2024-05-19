3c885a582239e953e8ad47c04e8de1d95806379c
shust3r <yardonesenchuk@icloud.com>
2024-05-19 20:57:29 +0300

    History Sidebar functionality improved

--- TaskBoard.UI/src/app/board-history/board-history.component.ts
+++ TaskBoard.UI/src/app/board-history/board-history.component.ts
@@ -1,12 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IActivity } from '../Interfaces/iactivity';
+import { ActivityService } from '../Services/activity.service';
+
 
 @Component({
   selector: 'app-board-history',
   templateUrl: './board-history.component.html',
   styleUrl: './board-history.component.css'
 })
-export class BoardHistoryComponent {
+export class BoardHistoryComponent implements OnInit {
   @Input() hideBtn : boolean;
+  activities: IActivity[];
+
+  constructor(
+    private actSvc: ActivityService
+  ) {}
+
+  ngOnInit(): void {
+    this.actSvc.getActivities().subscribe(response => {
+      this.activities = response;
+    });
+  }
 
   switchHistory() {
     this.hideBtn = true;
